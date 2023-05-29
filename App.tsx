a3babb833dd0e053d945bf5e06a1339ced25d78e
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2023-05-29 11:57:41 +0600

    set title dynamically with options prop

--- App.tsx
+++ App.tsx
@@ -5,15 +5,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import { RootStackParamList } from "./types/navigation";
+import { CATEGORIES } from "./data/dummy-data";
 export default function App() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MealsCategories">
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+          initialRouteName="MealsCategories"
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={({ navigation, route }) => {
+              const { categoryId } = route.params;
+              const category = CATEGORIES.find((c) => c.id === categoryId);
+              return {
+                title: category?.title,
+              };
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
